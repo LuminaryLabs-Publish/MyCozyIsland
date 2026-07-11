f92f6df68227d98a60c14478c37e5074e81456da
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-11 08:59:26 -0400

    Export lazy cell materializer

--- src/world/index.js
+++ src/world/index.js
@@ -2,5 +2,6 @@ export { COZY_WORLD_CONFIG, NEXUS_ENGINE_COMMIT } from "./world-config.js";
 export { createCozyIslandWorldRuntime } from "./world-runtime.js";
 export { createLegacyWorldComposition } from "./legacy-world-composition.js";
 export { createCozyWorldQuery } from "./world-query.js";
+export { createLazyCellMaterializer } from "./lazy-cell-materializer.js";
 export { createRendererCellCache } from "../kits/renderer-cell-cache.js";
 export { disposeRendererObject } from "../kits/renderer-disposal.js";
